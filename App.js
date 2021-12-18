32af9de19e5f6b93a4e23bf37867b8ce5397b60e
Uday Kiran Irukula <77915031+udaykiran-i@users.noreply.github.com>
2021-12-18 19:43:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import Users from "./components/data";
+import "./styles.css";
+import React, { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users_data: [], loading: false };
+    this.updateState = this.updateState.bind(this);
+  }
+  updateState() {
+    this.setState({ loading: true });
+
+    setTimeout(
+      async function () {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const jsonresponse = await response.json();
+        console.log(jsonresponse);
+        this.setState({ users_data: jsonresponse["data"], loading: false });
+      }.bind(this),
+      4000
+    );
+  }
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navitems">
+            <h2>LGMVIP Task2 Fetch data using Api</h2>
+            <button className="btn" onClick={this.updateState}>
+              Get Users
+            </button>
+          </div>
+        </nav>
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+        </div>
+      </>
+    );
+  }
+}
+export default App;
